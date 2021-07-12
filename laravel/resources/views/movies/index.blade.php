fefc6353d439d8ca76224c27982aff2784a0dc07
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-12 16:32:48 +0200

    All cards are now generated from db data

--- laravel/resources/views/movies/index.blade.php
+++ laravel/resources/views/movies/index.blade.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Movies DB</title>
 </head>
 <body>
-    @foreach ($movies as $movie)
-         {{$movie->title}}
-    @endforeach
+    <main>
+    <div class="container">
+        <div class="cards">
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <h2>{{$movie->title}}</h2>
+                    <h3>Original title: {{$movie->original_title}}</h3>
+                    <h3>Nationality: {{$movie->nationality}}</h3>
+                    <span>{{$movie->date}}</span>
+                    <h3>AVG Vote: {{$movie->vote}}</h3>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    </main>
 </body>
 </html>
