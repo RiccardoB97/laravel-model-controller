e6598f111fd4e321d6004d9b29a37fc7ab7538eb
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-12 15:54:23 +0200

    Add class model Movie and MoviesController page

--- laravel/app/Http/Controllers/MoviesController.php
+++ laravel/app/Http/Controllers/MoviesController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Movie;
 class MoviesController extends Controller
 {
-    //
-    
+    public function movies(){
+        $movies = Movie::all();
+        return view('movies.index',  compact('movies'));
+    }
+
 }
